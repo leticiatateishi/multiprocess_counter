25110c134db0299a69c376e22f364fae54dc8d02
leticiatateishi <leticiatateishi@gmail.com>
2019-05-10 14:15:50 -0300

    Adicionando lab 4

--- src/main.c
+++ src/main.c
@@ -1,17 +1,61 @@
-/* Contador de palavras
- *
- * Este programa recebera uma serie de caracteres representando palavras em sua
- * entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
- * numero de palavras separadas que recebeu e, apos, encerrar.
- */
-
 #include <stdio.h>
+#include <unistd.h>     // fork
+#include <sys/wait.h>   // waitpid
+#include <sys/mman.h>   // mmap
+
+#define NUM_PROCESSOS 4
+
+int primo(int numero){
+    if (numero < 2)
+        return 0;
+    for (int i = 2; i <= (numero/2); i++){
+        if (numero%i == 0)
+            return 0;
+    }
+    return 1;
+}
 
 int main() {
 
-  int x, y;
+    int protection = PROT_READ | PROT_WRITE;
+    int visibility = MAP_SHARED | MAP_ANON;
 
-  scanf("%d %d\n", &x, &y);
-  printf("%d\n", x + 200);
-  return 0;
+    int *mem;
+    mem = (int*) mmap(NULL, sizeof(int)*1000, protection, visibility, 0, 0);
+
+    int *posicao = &(mem[0]);
+    (*posicao) = 2;
+    int *resultado = &(mem[1]);
+
+    while(scanf(" %d ", &(mem[*posicao])) != EOF) {
+        (*posicao)++;
+    }
+
+    int pid_pai = getpid();
+
+    int pid[NUM_PROCESSOS];
+    for (int i = 0; i < NUM_PROCESSOS; i++) {
+        pid[i] = fork();
+        if (pid[i] == 0) {
+
+            break;
+        }
+    }
+
+    while (1){
+        int pos = (*posicao)--;
+        if (pos < 2)
+            break;
+        if (primo(mem[pos]) == 1)
+            (*resultado)++;
+    }
+
+    if (pid_pai == getpid()) {
+        for (int i = 0; i < NUM_PROCESSOS; i++) {
+            waitpid(pid[i], NULL, 0);
+        }
+        printf("%d\n", (*resultado));
+    }
+
+    return 0;
 }
